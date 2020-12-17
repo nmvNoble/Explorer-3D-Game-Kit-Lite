767a3e450b4e03e687fe65baa62c7209690bc64d
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-12-17 15:06:27 +0800

    Initial v0.2 set up
    Setted up new Scene for v0.1
    Initial script for toggling enemies on

---
+++ Assets/Scripts/EnemyToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyToggle : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _enemies; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Ellen")
+        {
+            foreach (GameObject e in _enemies)
+            {
+                e.SetActive(true);
+            }
+        }
+    }
+}
